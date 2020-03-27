cde51351d32ea7d5507b4529cd8ba31afdf8141f
Phattarin Kitbumrung <phattarin@magicboxasia.com>
2020-03-27 12:35:12 +0700

    set time

--- index.js
+++ index.js
@@ -7,19 +7,19 @@ const port = process.env.PORT || 3000
 app.get('/covid19', async(req, res) => {
   const thaidata = await getcountries()
   const date = new Date()
-  const time = date.getHours() + ':' + date.getMinutes()
+  //const time = date.getHours() + ':' + date.getMinutes()
   try {
     const notify = new lineapi.Notify({
         token: 'v9nOsQexb6EV0Gpenyb6hocwnOcqdSfwOB7yJRSYczD'
     })
-    if(time == '5:34'){ res.send("xxx")
+    //if(time == '10:10'){
         notify.send({
             message: 'Thailand Updated' + '\ncases:' + thaidata.cases + '\ntodayCases:' + thaidata.todayCases
             + '\ndeaths:' + thaidata.deaths + '\ntodayDeaths:' + thaidata.todayDeaths
             + '\nrecovered:' + thaidata.recovered + '\nactive:' + thaidata.active
             + '\ncritical:' + thaidata.critical
         }).then(res.send(thaidata))
-    }
+    //}
   } catch (error) {
       res.send(error)
   }
