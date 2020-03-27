9ca0508b42f147b4070a1e3218991d2cd1291dc9
Phattarin Kitbumrung <phattarin@magicboxasia.com>
2020-03-27 14:42:02 +0700

    set time

--- index.js
+++ index.js
@@ -4,22 +4,19 @@ const lineapi = require('line-api')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', async(req, res) => {
+app.get('/covid19', async(req, res) => {
   const thaidata = await getcountries()
-  //const date = new Date()
-  //const time = date.getHours() + ':' + date.getMinutes()
   try {
     const notify = new lineapi.Notify({
         token: 'v9nOsQexb6EV0Gpenyb6hocwnOcqdSfwOB7yJRSYczD'
     })
-    //if(time == '10:10'){
-        notify.send({
-            message: 'Thailand Updated' + '\ncases:' + thaidata.cases + '\ntodayCases:' + thaidata.todayCases
-            + '\ndeaths:' + thaidata.deaths + '\ntodayDeaths:' + thaidata.todayDeaths
-            + '\nrecovered:' + thaidata.recovered + '\nactive:' + thaidata.active
-            + '\ncritical:' + thaidata.critical
-        }).then(res.send(thaidata))
-    //}
+    notify.send({
+      message: 'Thailand Updated' + '\ncases:' + thaidata.cases + '\ntodayCases:' + thaidata.todayCases
+        + '\ndeaths:' + thaidata.deaths + '\ntodayDeaths:' + thaidata.todayDeaths
+        + '\nrecovered:' + thaidata.recovered + '\nactive:' + thaidata.active
+        + '\ncritical:' + thaidata.critical
+        + '\n\n developed by minizymint'
+    }).then(res.send(thaidata))
   } catch (error) {
       res.send(error)
   }
